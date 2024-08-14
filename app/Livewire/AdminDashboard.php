d68cf9915280af4e8787820e373f4b22b5940c31
Tiagokochem <tiagok989@gmail.com>
2024-08-13 21:51:23 -0300

    Breadcrumb

--- app/Livewire/AdminDashboard.php
+++ app/Livewire/AdminDashboard.php
@@ -4,10 +4,17 @@ namespace App\Livewire;
 
 use Livewire\Component;
 
+
 class AdminDashboard extends Component
 {
+    public $currentUrl;
+
     public function render()
     {
+
+        $current_url = url()->current();
+        $explode_url = explode('/', $current_url);
+        $this->currentUrl = $explode_url[3] . ' ' . $explode_url[4];
         return view('livewire.admin-dashboard')->layout('admin-layout');
     }
 }
